c696e4441a700184bbc8c8e08ee1df49b34fb4fa
notpritam <119423845+notpritam@users.noreply.github.com>
2023-08-01 19:04:22 +0530

    Design

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <header>
+        <span className="uppercase text-[14px]">Virtual Gallery</span>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
