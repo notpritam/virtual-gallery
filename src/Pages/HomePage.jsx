34b4b90985c0124ec22aa6ec3574b6d2423e8575
notpritam <119423845+notpritam@users.noreply.github.com>
2023-08-02 15:42:24 +0530

    Design

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import star from "../assets/star.svg";
 import happyFace from "../assets/happyFace.svg";
 
@@ -8,7 +8,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 function HomePage() {
   //   const sectionRef = useRef(null);
   //   const triggerRef = useRef(null);
-  //   const imageRef = useRef(null);
+  const imageRef = useRef(null);
 
   //   gsap.registerPlugin(ScrollTrigger);
 
@@ -39,38 +39,84 @@ function HomePage() {
   //     };
   //   }, []);
 
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(".happyFace", {
+      rotate: 360,
+      duration: 1.5,
+      repeat: -1,
+      ease: "linear",
+    });
+
+    gsap.to("._heroHeader", {
+      top: "-110%",
+      duration: 2.5,
+      ease: "expo.easeout",
+
+      scrollTrigger: {
+        trigger: imageRef.current,
+        start: "center-10% center",
+        // end: "bottom bottom",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    // gsap.to("_image", {
+    //   scale: 1,
+    //   duration: 2,
+    //   scrollTrigger: {
+    //     trigger: "._heroHeader",
+    //     start: "top top",
+    //     // end: "bottom top",
+    //     scrub: true,
+    //     markers: true,
+    //   },
+    // });
+  }, []);
+
   return (
     <>
-      <div className="h-[70vh] flex-col justify-between flex border-b-[1px] border-borderColor">
-        <header className="fixed top-0 left-0 right-0 w-full pl-[1.5rem] pr-[1.5rem] flex justify-between items-center p-[1rem] border-b-[1px] border-borderColor">
-          <span className="uppercase text-[14px]">Virtual Gallery</span>
-          <span className="uppercase text-[14px]">Thank You!!</span>
-        </header>
-        <div className="flex  flex-col justify-center items-center w-[80%] m-auto">
-          <img className="h-[1.75rem] w-[1.75rem]" src={star}></img>
-          <span className="text-[2.2857rem] leading-[2.2857rem]">
-            PHOTOGRAPHER'S
-          </span>
-          <span className="text-[7.1429rem] leading-[7.1429rem]">
-            CELEBRATION
-          </span>
-          <span className="w-[40%] text-center text-[14px] ">
-            TODAY WE WANT TO HONOR ALL THE PHOTOGRAPHERS WHO GENEROUSLY SHARE
-            THEIR PICTURES WITH THE WORLD. THIS WEBSITE IS A CELEBRATION OF
-            THEIR TALENT!
-          </span>
+      <header className="fixed z-[999] top-0 left-0 right-0 w-full pl-[1.5rem] pr-[1.5rem] flex justify-between items-center p-[1rem] border-b-[1px] border-borderColor">
+        <span className="uppercase text-[14px]">Virtual Gallery</span>
+        <span className="uppercase text-[14px]">Thank You!!</span>
+      </header>
+      <div>
+        <div className="h-[70vh] top-0 absolute bg-background w-full _heroHeader flex-col justify-between flex border-b-[1px] border-borderColor">
+          <div className="flex  flex-col justify-center items-center w-[80%] m-auto">
+            <img className="h-[1.75rem] w-[1.75rem]" src={star}></img>
+            <span className="text-[2.2857rem] leading-[2.2857rem]">
+              PHOTOGRAPHER'S
+            </span>
+            <span className="text-[7.1429rem] leading-[7.1429rem]">
+              CELEBRATION
+            </span>
+            <span className="w-[40%] text-center text-[14px] ">
+              TODAY WE WANT TO HONOR ALL THE PHOTOGRAPHERS WHO GENEROUSLY SHARE
+              THEIR PICTURES WITH THE WORLD. THIS WEBSITE IS A CELEBRATION OF
+              THEIR TALENT!
+            </span>
+          </div>
+          <div className="p-4 flex gap-2 items-center">
+            <span className="uppercase text-[14px]">Enjoy Your Visit</span>
+            <img
+              className="h-[1.5rem] w-[1.5rem] happyFace"
+              src={happyFace}
+            ></img>
+          </div>
         </div>
-        <div className="p-4 flex gap-2 items-center">
-          <span className="uppercase text-[14px]">Enjoy Your Visit</span>
-          <img className="h-[1.5rem] w-[1.5rem]" src={happyFace}></img>
-        </div>
-      </div>
 
-      <div className="h-screen overflow-hidden">
-        <img
-          className="object-cover object-center h-full w-full"
-          src="https://virtual-gallery.okeystudio.com/photo1.42abc711.webp"
-        ></img>
+        <div
+          className="h-[120vh] w-full mt-[10rem]   _heroImage overflow-hidden"
+          ref={imageRef}
+        >
+          <div className="h-full w-full overflow-hidden">
+            <img
+              className="object-cover object-center h-full w-full  _image"
+              src="https://virtual-gallery.okeystudio.com/photo1.42abc711.webp"
+            ></img>
+          </div>
+        </div>
       </div>
     </>
   );
